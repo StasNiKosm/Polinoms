7fd16e88f3fb368932717fc3e0b0363e546187ba
Makovetsky Artem <42765232+Jukert@users.noreply.github.com>
2019-03-17 18:41:38 +0300

    Check
    если ты пишешь Check, то мето должен по своему происхождению возвращать true/false

--- src/main/java/parsingPolinom/CheckPower.java
+++ src/main/java/parsingPolinom/CheckPower.java
@@ -1,13 +1,15 @@
 package parsingPolinom;
 
 public class CheckPower {
-    public static int checkPower (String monomial) {
+    
+    public static boolean checkPower (String monomial) {
         if(monomial.contains("^")) {
            return Integer.parseInt(monomial.substring(monomial.indexOf("^") + 1));
         } else if (monomial.contains("x")) {
-            return 1;
+            return true;
         } else {
-            return 0;
+            return false;
         }
     }
+    
 }
