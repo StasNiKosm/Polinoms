a00a8f5c2a11676546ad5e2bf310406fe2aae851
Makovetsky Artem <42765232+Jukert@users.noreply.github.com>
2019-03-17 18:39:44 +0300

    Офоромление
    Всегда в if пиши фигурные скобки, код становиться намного читабельнее
    Позже приложу своё видение по реализации данног метода, слишком нагроможден проверками

--- src/main/java/parsingPolinom/CheckCoeff.java
+++ src/main/java/parsingPolinom/CheckCoeff.java
@@ -2,14 +2,20 @@ package parsingPolinom;
 
 public class CheckCoeff {
 
-    public static int checkCoeff (String monomial) {
+    public static boolean checkCoeff (String monomial) {
         if(monomial.contains("x")) {
-            if(monomial.startsWith("x")) return 1;
+            if(monomial.startsWith("x")) {
+                return true;
+            }
             else {
                 if (monomial.contains("{")) {
                     String[] monomialParts = monomial.split("x");
-                    if (monomialParts[0].endsWith("}")) return 1;
-                    else return Integer.parseInt(monomialParts[0].substring(3));
+                    if (monomialParts[0].endsWith("}")) {
+                        return 1;
+                    }
+                    else {
+                        return Integer.parseInt(monomialParts[0].substring(3));
+                    }
                 } else  {
                     String[] monomialParts = monomial.split("x");
                     return Integer.parseInt(monomialParts[0]);
@@ -17,9 +23,15 @@ public class CheckCoeff {
             }
         } else {
             if(monomial.contains("{")) {
-                if(Integer.parseInt(monomial.substring(3)) == 0) return 1;
-                else return Integer.parseInt(monomial.substring(3));
-            } else return Integer.parseInt(monomial);
+                if(Integer.parseInt(monomial.substring(3)) == 0) {
+                    return 1;
+                }
+                else {
+                    return Integer.parseInt(monomial.substring(3));
+                }
+            } else {
+                return Integer.parseInt(monomial);
+            }
         }
     }
 }
